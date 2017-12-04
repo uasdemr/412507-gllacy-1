21c8da76055bdea686c6e2f3ad11902475da1409
uasdemr <sdyuzhilin@mail.ru>
2017-12-04 19:39:17 +0300

    доработал отображение формы

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 var button = document.querySelector(".map-contacts-btn");
+var formFlag = 0;
 
 button.addEventListener("click", function () {
   var modalForm = document.querySelector(".modal-feedback");
-  modalForm.style.display = "block";
+  if(formFlag == 0) {
+    formFlag = 1;
+    modalForm.style.display = "block";
+  }
 });
 
 var modalClose = document.querySelector(".modal-close");
 
 modalClose.addEventListener("click", function () {
   var modalForm = document.querySelector(".modal-feedback");
-  modalForm.style.display = "none";
+  if(formFlag == 1) {
+    formFlag = 0;
+    modalForm.style.display = "none";
+  }
 });
